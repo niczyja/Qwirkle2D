267925638643faf9f5403a5f0fe2c4a50b7fbfb5
Maciej Sienkiewicz <maciej@niczyja.pl>
2023-12-27 22:49:04 +0100

    add project

---
+++ Qwirkle2D/Qwirkle2D/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Qwirkle2D
+//
+//  Created by Maciej Sienkiewicz on 27/12/2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
